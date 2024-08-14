1c7bf1ab70381db28824d59ab96563d95802a360
AlITOKMAK35 <145210127+AlITOKMAK35@users.noreply.github.com>
2024-08-15 00:07:09 +0300

    CSharp ile Klavye Uygulaması
    C# ile Klavye Uygulaması, klavye girişlerini izleyen, yöneten veya özelleştiren bir yazılımdır. İşlevleri şunları içerebilir:
    
    Klavye Giriş İzleme: Tuş vuruşlarını takip etme.
    Klavye Kısayolları: Kısayollar tanımlama ve özelleştirme.
    Klavye Haritalama: Tuş işlevlerini yeniden düzenleme.
    Klavye Kayıtları ve Analiz: Girişleri kaydetme ve analiz etme.
    Uyarılar ve Bildirimler: Belirli tuş kombinasyonlarına tepki verme.
    Bu uygulama genellikle verimliliği artırma, kullanıcı davranışını izleme veya güvenliği sağlama amacıyla kullanılır.

---
+++ klavye/klavye/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace klavye
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            textBox1.Text = textBox1.Text + b.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        if (textBox1.Text.Length > 0)
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+            }
+        }
+        bool CapsLock = true;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (CapsLock)
+            {
+                CapsLock = false;
+                lbl_bilgi.Text = lbl_bilgi.Text.ToLower();
+            }
+            else
+            {
+                CapsLock = true;
+                lbl_bilgi.Text = lbl_bilgi.Text.ToUpper();
+            }
+            foreach ( Button item in panel1.Controls )
+            {
+                if (CapsLock)
+                    item.Text = item.Text.ToUpper();
+                else
+                    item.Text = item.Text.ToLower();
+            }
+        }
+
+        
+    }
+}
